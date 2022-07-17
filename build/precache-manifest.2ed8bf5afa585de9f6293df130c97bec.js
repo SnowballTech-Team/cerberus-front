314912bba2b0df7ccf55246d4423e8073b606f24
wilson <745105750@qq.com>
2022-07-18 01:19:32 +0800

    打包

---
+++ build/precache-manifest.2ed8bf5afa585de9f6293df130c97bec.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf80effd908a9d2e8d17",
+    "url": "/static/css/main.01381e86.chunk.css"
+  },
+  {
+    "revision": "bf80effd908a9d2e8d17",
+    "url": "/static/js/main.bf80effd.chunk.js"
+  },
+  {
+    "revision": "dabb5bb1f5963f528fd8",
+    "url": "/static/js/1.dabb5bb1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "991dfacc06b157a7e4970574cfbf03a3",
+    "url": "/static/media/logo.991dfacc.svg"
+  },
+  {
+    "revision": "84ca773867a7f1c8b9fc20770a2cb20c",
+    "url": "/static/media/bg-gradient-img.84ca7738.png"
+  },
+  {
+    "revision": "701dfcba945266fcb9202ef3b0a3a6cf",
+    "url": "/static/media/tw2.701dfcba.svg"
+  },
+  {
+    "revision": "733f26ebc5c38715b337115a030b0a6d",
+    "url": "/static/media/tg2.733f26eb.svg"
+  },
+  {
+    "revision": "d2d2891be4045635e137b6bdd9b3e910",
+    "url": "/static/media/git2.d2d2891b.svg"
+  },
+  {
+    "revision": "ec998ac0b802dc24ee57aea6de86189a",
+    "url": "/static/media/med2.ec998ac0.svg"
+  },
+  {
+    "revision": "4e722d4c7a499c09a1e03a01bd8d2a8f",
+    "url": "/static/media/list_bg.4e722d4c.png"
+  },
+  {
+    "revision": "cce49cbf167954f001521c01dbd2bc68",
+    "url": "/static/media/cdoge.cce49cbf.png"
+  },
+  {
+    "revision": "eb5417e7c3bc362c2c6384aabe67cc9d",
+    "url": "/static/media/cnft.eb5417e7.png"
+  },
+  {
+    "revision": "1981d510a1c6c853a409819c31bce1d8",
+    "url": "/static/media/ctaik.1981d510.png"
+  },
+  {
+    "revision": "cfd30649543de786c55806d104686e70",
+    "url": "/static/media/cswap.cfd30649.png"
+  },
+  {
+    "revision": "f32cff512f33a8acb966dd2411287d39",
+    "url": "/static/media/beruscoin.f32cff51.png"
+  },
+  {
+    "revision": "ffbf8c752534a1b57132ab8ef307083c",
+    "url": "/static/media/romap.ffbf8c75.png"
+  },
+  {
+    "revision": "4dc1e0edd17dd9d0bec07e3a155ea18a",
+    "url": "/static/media/block4_bg.4dc1e0ed.png"
+  },
+  {
+    "revision": "77f4171156b26f16047b9a55bfc6f44b",
+    "url": "/static/media/big_bg.77f41711.png"
+  },
+  {
+    "revision": "523aaf22259007d36f27b913eac27d48",
+    "url": "/static/media/tw.523aaf22.svg"
+  },
+  {
+    "revision": "3e331e6a98528902854378ecb8af5d7a",
+    "url": "/static/media/tg.3e331e6a.svg"
+  },
+  {
+    "revision": "84a0dbd8a3ad0b1a3683fb189c40d789",
+    "url": "/static/media/git.84a0dbd8.svg"
+  },
+  {
+    "revision": "57c8cfdc94431ac828a9c5fdfa7a694b",
+    "url": "/static/media/med.57c8cfdc.svg"
+  },
+  {
+    "revision": "d8ba430e0a461c33f489975a153829c5",
+    "url": "/static/media/doge.d8ba430e.png"
+  },
+  {
+    "revision": "494c787cecd113e7ebed6adfd839af75",
+    "url": "/static/media/images.494c787c.png"
+  },
+  {
+    "revision": "be7bbfd44f5587b6d25f0aef01b747ad",
+    "url": "/static/media/bitcoin.be7bbfd4.png"
+  },
+  {
+    "revision": "bf05b699a36ff217d594304dbab21542",
+    "url": "/static/media/block1.bf05b699.mp4"
+  },
+  {
+    "revision": "59cb1ee8035af43375d7f2f2b66fd592",
+    "url": "/static/media/center_bg.59cb1ee8.png"
+  },
+  {
+    "revision": "257a10da95cc869ba9a8f2a76710d4fb",
+    "url": "/static/media/block_bg.257a10da.png"
+  },
+  {
+    "revision": "07ffee16956b317336dbbe57cbb3877a",
+    "url": "/index.html"
+  }
+];
