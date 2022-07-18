d1bd1a1bb0398d58938783769abb8c98c0fa6cb1
ClearFish <xiangtao@wuhandsj.com>
2022-07-18 09:52:27 +0800

    注释APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import * as React from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
 import { HomeLayout } from "./components/HomeLayout";
 function App() {
   return (
-    // <Router>
-    //   <Route exact path="/">
-    //     <HomeLayout>
-    //       <Home/>
-    //     </HomeLayout>
-    //   </Route>
-    //   <Route path="/home" component={Home}/>
-    // </Router>
     <div>
       <HomeLayout />
     </div>
